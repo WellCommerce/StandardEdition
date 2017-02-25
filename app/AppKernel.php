38f5ee42cff6c5745e184857962b9bdc34fa4695
Adam Piotrowski <adam@wellcommerce.org>
2017-02-25 01:55:11 +0100

    Prepared standard distro

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -10,35 +10,18 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpKernel\Kernel;
+use WellCommerce\Bundle\CoreBundle\HttpKernel\WellCommerceKernel;
 
 /**
  * Class AppKernel
  *
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  */
-class AppKernel extends Kernel
+class AppKernel extends WellCommerceKernel
 {
     public function registerBundles()
     {
         $bundles = [
-            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new \Symfony\Bundle\TwigBundle\TwigBundle(),
-            new \Symfony\Bundle\MonologBundle\MonologBundle(),
-            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new \Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle(),
-            new \Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new \FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new \Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle(),
-            new \Liip\ImagineBundle\LiipImagineBundle(),
-            new \Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle(),
-            new \Cache\AdapterBundle\CacheAdapterBundle(),
-            new \EmanueleMinotto\TwigCacheBundle\TwigCacheBundle(),
             new \WellCommerce\Bundle\ApiBundle\WellCommerceApiBundle(),
             new \WellCommerce\Bundle\AppBundle\WellCommerceAppBundle(),
             new \WellCommerce\Bundle\CoreBundle\WellCommerceCoreBundle(),
@@ -55,20 +38,6 @@ class AppKernel extends Kernel
             new \WellCommerce\Bundle\WishlistBundle\WellCommerceWishlistBundle(),
         ];
         
-        if (in_array($this->getEnvironment(), ['dev', 'test'])) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-
-            if ($this->getEnvironment() === 'test') {
-                $bundles[] = new \DAMA\DoctrineTestBundle\DAMADoctrineTestBundle();
-            }
-        }
-        
-        return $bundles;
-    }
-    
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+        return array_merge(parent::registerBundles(), $bundles);
     }
 }
